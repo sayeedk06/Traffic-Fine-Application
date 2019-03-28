50b67b5ac19354f94839c40faa8d879d04457804
AtiqueNSU <39723702+AtiqueNSU@users.noreply.github.com>
2019-03-28 14:35:36 +0600

    CameraWorking

--- app/src/main/java/com/example/trafficpoliceassistance/MainActivity.java
+++ app/src/main/java/com/example/trafficpoliceassistance/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.trafficpoliceassistance;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import   android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
